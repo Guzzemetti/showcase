d6bd40a1572902cf8d957d3882914289d8069969
John Guzzetta <johnguzzettaiv@gmail.com>
2022-06-05 14:59:34 -0400

    Added in basic About Me section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import Nav from './components/Nav/Navbar';
 import Projects from './components/Projs/Project';
+import About from './components/About/About';
 
 function App() {
   return (
     <>
     <Nav />
+    <About />
     <Projects /> 
     </>
   );
---
+++ portfolio/src/components/About/About.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './About.css'
+
+function About(){
+    return (
+        <>
+        <div className='container'>
+            <div>
+                <h1> About Me</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odit accusantium expedita maiores ipsum recusandae doloremque sapiente alias, dolores nam provident aut suscipit vel quis eos, libero perferendis magnam? Dolorum.</p>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default About;
