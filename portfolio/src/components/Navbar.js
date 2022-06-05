bea31e999fb57e1d1abf4aa1e8d7a6676e95d6f0
John Guzzetta <johnguzzettaiv@gmail.com>
2022-06-05 13:04:43 -0400

    Finally got my navbar to work properly

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navbar, NavbarBrand, NavbarText, NavLink } from 'reactstrap';
+
+
+function Nav() {
+return (
+    <>
+<div>
+  <Navbar
+    color="secondary"
+    dark
+    expand="md"
+    light
+  >
+    <NavbarBrand href="/">
+      John Guzzetta IV
+    </NavbarBrand>
+    {/* <NavLink href='www.publix.com'>
+    Publix
+    </NavLink> */}
+  </Navbar>
+</div>
+</>
+);
+}
+
+export default Nav;
