afcb7ff06e8309385e3847c6be7bfd8a19be2bd7
John Guzzetta <johnguzzettaiv@gmail.com>
2022-06-05 22:57:26 -0400

    Added a row for the About Me

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './About.css'
+import './About.css';
+import { Col, Row } from 'reactstrap'
 
 function About(){
     return (
@@ -7,6 +8,28 @@ function About(){
         <div className='container'>
             <div>
                 <h1> About Me</h1>
+                <Row>
+    <Col
+      className="bg-light border"
+      sm="4"
+      xs="6"
+    >
+      .col-6 .col-sm-4
+    </Col>
+    <Col
+      className="bg-light border"
+      sm="4"
+      xs="6"
+    >
+      .col-6 .col-sm-4
+    </Col>
+    <Col
+      className="bg-light border"
+      sm="4"
+    >
+      .col-sm-4
+    </Col>
+  </Row>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odit accusantium expedita maiores ipsum recusandae doloremque sapiente alias, dolores nam provident aut suscipit vel quis eos, libero perferendis magnam? Dolorum.</p>
             </div>
         </div>
